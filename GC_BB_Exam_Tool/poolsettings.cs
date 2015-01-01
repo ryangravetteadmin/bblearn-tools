1aed75a06df2f3fe58d59ba1ec4996d378e958e1
Philip Bawn <pabawn@gmail.com>
2014-12-31 22:57:36 -0800

    added meaningful comments to code.

--- GC_BB_Exam_Tool/poolsettings.cs
+++ GC_BB_Exam_Tool/poolsettings.cs
@@ -17,23 +17,27 @@ namespace GC_BB_Exam_Tool
         public poolsettings()
         {
             InitializeComponent();
+            // Allow drag and dropping onto the pool settings form.
             this.AllowDrop = true;
+            // Create event handlers for drag and drop events
             this.DragEnter += new DragEventHandler(poolsettings_DragEnter);
             this.DragDrop += new DragEventHandler(poolsettings_DragDrop);
         }
 
         void poolsettings_DragEnter(object sender, DragEventArgs e)
         {
+            // The drag enter event....
             if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
         }
 
         void poolsettings_DragDrop(object sender, DragEventArgs e)
         {
+            // One or more files just got dropped anywhere within this window...
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
             foreach (string file in files)
             {
                 // each File is a zip file dropped on this window
-                //MessageBox.Show(file);
+                // Exctract each zip file:
                 string extractDir = System.IO.Path.GetDirectoryName(file) + "\\" + System.IO.Path.GetFileNameWithoutExtension(file);
                 System.IO.Directory.CreateDirectory(extractDir);
                 ZipFile.ExtractToDirectory(file, extractDir);
