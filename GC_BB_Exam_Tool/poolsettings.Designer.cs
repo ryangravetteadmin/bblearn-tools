e5d0709f0b4415db769002cac602884a02225f8f
Philip Bawn <pabawn@gmail.com>
2015-01-01 02:32:48 -0800

    handling file drops now, identifying what is in them. There are still bugs with deciding what is in the blackboard files, though. I wanted to get the code up to github

--- GC_BB_Exam_Tool/poolsettings.Designer.cs
+++ GC_BB_Exam_Tool/poolsettings.Designer.cs
@@ -32,15 +32,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panelPool = new System.Windows.Forms.Panel();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.QuestionText = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Numbering = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Orientation = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.PartialCredit = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.RandomizeAnswers = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.lblNumberOfMultipleChoiceInPool = new System.Windows.Forms.Label();
             this.btnHelp = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.btnSavePool = new System.Windows.Forms.Button();
+            this.lblShuffled = new System.Windows.Forms.Label();
+            this.lblPartialCredit = new System.Windows.Forms.Label();
             this.panelPool.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,6 +55,7 @@
             this.label1.Size = new System.Drawing.Size(610, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -63,62 +68,32 @@
             // panelPool
             // 
             this.panelPool.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panelPool.Controls.Add(this.listView1);
+            this.panelPool.Controls.Add(this.lblPartialCredit);
+            this.panelPool.Controls.Add(this.lblShuffled);
+            this.panelPool.Controls.Add(this.btnSavePool);
+            this.panelPool.Controls.Add(this.checkBox2);
+            this.panelPool.Controls.Add(this.checkBox1);
+            this.panelPool.Controls.Add(this.comboBox2);
+            this.panelPool.Controls.Add(this.label4);
+            this.panelPool.Controls.Add(this.comboBox1);
+            this.panelPool.Controls.Add(this.label3);
+            this.panelPool.Controls.Add(this.lblStatus);
             this.panelPool.Controls.Add(this.lblNumberOfMultipleChoiceInPool);
             this.panelPool.Location = new System.Drawing.Point(11, 58);
             this.panelPool.Name = "panelPool";
-            this.panelPool.Size = new System.Drawing.Size(741, 237);
+            this.panelPool.Size = new System.Drawing.Size(741, 285);
             this.panelPool.TabIndex = 2;
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.QuestionText,
-            this.Numbering,
-            this.Orientation,
-            this.PartialCredit,
-            this.RandomizeAnswers});
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(14, 31);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(715, 191);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // QuestionText
-            // 
-            this.QuestionText.Text = "Question";
-            this.QuestionText.Width = 334;
-            // 
-            // Numbering
-            // 
-            this.Numbering.Text = "Numbering Style";
-            this.Numbering.Width = 94;
-            // 
-            // Orientation
-            // 
-            this.Orientation.Text = "Orientation";
-            this.Orientation.Width = 84;
-            // 
-            // PartialCredit
-            // 
-            this.PartialCredit.Text = "Partial Credit";
-            this.PartialCredit.Width = 87;
-            // 
-            // RandomizeAnswers
-            // 
-            this.RandomizeAnswers.Text = "Randomize Answers";
-            this.RandomizeAnswers.Width = 111;
+            this.panelPool.Visible = false;
+            this.panelPool.Paint += new System.Windows.Forms.PaintEventHandler(this.panelPool_Paint);
             // 
             // lblNumberOfMultipleChoiceInPool
             // 
             this.lblNumberOfMultipleChoiceInPool.AutoSize = true;
-            this.lblNumberOfMultipleChoiceInPool.Location = new System.Drawing.Point(17, 11);
+            this.lblNumberOfMultipleChoiceInPool.Location = new System.Drawing.Point(48, 57);
             this.lblNumberOfMultipleChoiceInPool.Name = "lblNumberOfMultipleChoiceInPool";
-            this.lblNumberOfMultipleChoiceInPool.Size = new System.Drawing.Size(173, 13);
+            this.lblNumberOfMultipleChoiceInPool.Size = new System.Drawing.Size(98, 13);
             this.lblNumberOfMultipleChoiceInPool.TabIndex = 0;
-            this.lblNumberOfMultipleChoiceInPool.Text = "n Multiple Choice Questions in Pool";
+            this.lblNumberOfMultipleChoiceInPool.Text = "n Questions in Pool";
             // 
             // btnHelp
             // 
@@ -132,16 +107,108 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 301);
+            this.listBox1.Location = new System.Drawing.Point(12, 362);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(729, 121);
             this.listBox1.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Maroon;
+            this.lblStatus.Location = new System.Drawing.Point(6, 11);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(726, 32);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "label3";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numbering type:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(144, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Answer orientation:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(147, 118);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(109, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(51, 152);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Randomize order of answers";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(51, 182);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(111, 17);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "Allow partial credit";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // btnSavePool
+            // 
+            this.btnSavePool.Location = new System.Drawing.Point(51, 218);
+            this.btnSavePool.Name = "btnSavePool";
+            this.btnSavePool.Size = new System.Drawing.Size(634, 46);
+            this.btnSavePool.TabIndex = 10;
+            this.btnSavePool.Text = "Change all question settings";
+            this.btnSavePool.UseVisualStyleBackColor = true;
+            // 
+            // lblShuffled
+            // 
+            this.lblShuffled.AutoSize = true;
+            this.lblShuffled.Location = new System.Drawing.Point(217, 153);
+            this.lblShuffled.Name = "lblShuffled";
+            this.lblShuffled.Size = new System.Drawing.Size(74, 13);
+            this.lblShuffled.TabIndex = 11;
+            this.lblShuffled.Text = "n of q shuffled";
+            // 
+            // lblPartialCredit
+            // 
+            this.lblPartialCredit.AutoSize = true;
+            this.lblPartialCredit.Location = new System.Drawing.Point(218, 182);
+            this.lblPartialCredit.Name = "lblPartialCredit";
+            this.lblPartialCredit.Size = new System.Drawing.Size(121, 13);
+            this.lblPartialCredit.TabIndex = 12;
+            this.lblPartialCredit.Text = "n of q allow partial credit";
             // 
             // poolsettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(764, 432);
+            this.ClientSize = new System.Drawing.Size(764, 495);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnHelp);
             this.Controls.Add(this.panelPool);
@@ -161,14 +228,18 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panelPool;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader QuestionText;
-        private System.Windows.Forms.ColumnHeader Numbering;
-        private System.Windows.Forms.ColumnHeader Orientation;
-        private System.Windows.Forms.ColumnHeader PartialCredit;
-        private System.Windows.Forms.ColumnHeader RandomizeAnswers;
         private System.Windows.Forms.Label lblNumberOfMultipleChoiceInPool;
         private System.Windows.Forms.Button btnHelp;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSavePool;
+        private System.Windows.Forms.Label lblPartialCredit;
+        private System.Windows.Forms.Label lblShuffled;
     }
 }
--- GC_BB_Exam_Tool/poolsettings.cs
+++ GC_BB_Exam_Tool/poolsettings.cs
@@ -15,6 +15,8 @@ namespace GC_BB_Exam_Tool
 {
     public partial class poolsettings : Form
     {
+        string poolfile;
+        string searchTerm;
         public poolsettings()
         {
             InitializeComponent();
@@ -33,68 +35,128 @@ namespace GC_BB_Exam_Tool
 
         void poolsettings_DragDrop(object sender, DragEventArgs e)
         {
-            // One or more files just got dropped anywhere within this window...
-            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-            foreach (string file in files)
-            {
-                // each File is a zip file dropped on this window
-                // at this point we could Exctract each zip file and do things with them, but by enclosing
-                // the code in the below if...statement, only the first file will be processed:
-                if (files.First() == file)
+            //try
+            //{
+                // One or more files just got dropped anywhere within this window...
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
                 {
-                    // Read from single zip file
-                    string extractDir = System.IO.Path.GetDirectoryName(file) + "\\" + System.IO.Path.GetFileNameWithoutExtension(file);
-                    System.IO.Directory.CreateDirectory(extractDir);
-                    ZipFile.ExtractToDirectory(file, extractDir);
-                    // Create an XML reader for the blackboard manifest file (
-                    using (XmlReader xreader = XmlReader.Create(extractDir + "\\imsmanifest.xml"))
+                    // each File is a zip file dropped on this window
+                    // at this point we could Exctract each zip file and do things with them, but by enclosing
+                    // the code in the below if...statement, only the first file will be processed:
+                    if (files.First() == file)
                     {
-                        while (xreader.Read())
+                        // Read from single zip file
+                        string extractDir = System.IO.Path.GetDirectoryName(file) + "\\" + System.IO.Path.GetFileNameWithoutExtension(file) + "-gctool";
+                        // Atempt to delete directory that question pool was extracted to
+                        System.IO.Directory.CreateDirectory(extractDir);
+                        ZipFile.ExtractToDirectory(file, extractDir);
+                        // Create an XML reader for the blackboard manifest file (
+                        using (XmlReader xreader = XmlReader.Create(extractDir + "\\imsmanifest.xml"))
                         {
-                            // Look for start of XML elements.
-                            switch (xreader.Name)
+                            while (xreader.Read())
                             {
-                            // ----------------------------------------------------------------
-                                case "resource":
-                                    //MessageBox.Show("Start of <resource> element.");
-                                    string filenameAttribute = xreader["bb:file"]; // File
-                                    string filetypeAttribute = xreader["type"];
-                                    string filecontentsTitle = xreader["bb:title"];
-                                    if (filenameAttribute != null)
-                                    {
-                                        switch (filetypeAttribute)
+                                // Look for start of XML elements.
+                                switch (xreader.Name)
+                                {
+                                    // ----------------------------------------------------------------
+                                    case "resource":
+                                        //MessageBox.Show("Start of <resource> element.");
+                                        string filenameAttribute = xreader["bb:file"]; // File
+                                        string filetypeAttribute = xreader["type"];
+                                        string filecontentsTitle = xreader["bb:title"];
+                                        if (filenameAttribute != null)
                                         {
-                                            case "assessment/x-bb-qti-pool":
-                                            int questionsInFile;
-                                            using (XmlReader questionReader = XmlReader.Create(extractDir + "\\imsmanifest.xml"))
+                                            switch (filetypeAttribute)
                                             {
-                                                while (questionReader.Read())
-                                                {
-                                                    // Look for start of XML elements.
-                                                    switch (questionReader.Name)
+                                                case "assessment/x-bb-qti-pool":
+                                                    int questionsInFile = 0;
+                                                    using (XmlReader questionReader = XmlReader.Create(extractDir + "\\" + filenameAttribute))
                                                     {
-                                                        case "presentation":
-                                                            int questionsInFile = questionsInFile+1;
-                                                            break;
+                                                        while (questionReader.Read())
+                                                        {
+                                                            // Look for start of XML elements.
+                                                            switch (questionReader.Name)
+                                                            {
+                                                                case "flow":
+                                                                    string flowClass = questionReader["class"];
+                                                                    // Blackboard keeps each question inside an element that looks like <flow class="QUESTION_BLOCK"....
+                                                                    if (string.Equals(flowClass, "question_block", StringComparison.CurrentCultureIgnoreCase))
+                                                                    {
+                                                                        questionsInFile++;
+                                                                    }
+                                                                    break;
+                                                                default:
+                                                                    // no special scenario going on
+                                                                    break;
+                                                            }
+                                                        }
                                                     }
-                                                }
+                                                    poolfile = extractDir + "\\" + filenameAttribute;
+                                                    listBox1.Items.Add("Found and counted questions in pool file: " + extractDir + "\\" + filenameAttribute);
+                                                    listBox1.Items.Add("     > Contains question pool '" + filecontentsTitle + "' (" + questionsInFile + ") questions.");
+                                                    panelPool.Visible = true;
+                                                    lblNumberOfMultipleChoiceInPool.Text = questionsInFile + " questions in pool '" + filecontentsTitle + "'. Change settings for all:";
+                                                    lblStatus.Text = "Working with: " + poolfile;
+                                                    // Use a new stream reader to read contents of a file into a string
+                                                    StreamReader sr = new StreamReader(poolfile, Encoding.Default);
+                                                    // read entire file
+                                                    string strz = sr.ReadToEnd();
+                                                    // close the stream
+                                                    sr.Close();
+
+                                                    // EXAMPLE : http://msdn.microsoft.com/en-us/library/bb546166.aspx
+                                                    // (How to use a LINQ query to count # of occurrences of a word in a string)
+                                                    //Convert the string (strz) into an array of words 
+                                                    string[] source = strz.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                                                    searchTerm = "shuffle=\"YES\"";
+                                                    // Create the query.  Use ToLowerInvariant to match "data" and "Data"  
+                                                    var matchQuery = from word in source
+                                                                     where word.ToLowerInvariant() == searchTerm.ToLowerInvariant()
+                                                                     select word;
+                                                    // Count the matches, which executes the query. 
+                                                    int wordCount = matchQuery.Count();
+                                                    lblShuffled.Text = wordCount + " of " + questionsInFile + " set to shuffle.";
+
+                                                    
+                                                    searchTerm = "<bbmd_partialcredit>true</bbmd_partialcredit>";
+                                                    // Create the query.  Use ToLowerInvariant to match "data" and "Data"  
+                                                    matchQuery = from word in source
+                                                                     where word.ToLowerInvariant() == searchTerm.ToLowerInvariant()
+                                                                     select word;
+                                                    // Count the matches, which executes the query. 
+                                                    wordCount = matchQuery.Count();
+                                                    lblPartialCredit.Text = wordCount + " of " + questionsInFile + " allow partial credit.";
+                                                    //
+                                                    break;
                                             }
-                                            listBox1.Items.Add("Found and counted questions in pool file: " + extractDir + "\\" + filenameAttribute);
-                                            listBox1.Items.Add("     > Contains question pool '" + filecontentsTitle + "' (" + questionsInFile + ") questions.");
-                                            break;
                                         }
-                                    }
-                                    break;
-                            // ----------------------------------------------------------------
+                                        break;
+                                    // ----------------------------------------------------------------
+                                }
+                                listBox1.TopIndex = listBox1.Items.Count - 1;
                             }
-                            listBox1.TopIndex = listBox1.Items.Count - 1;
                         }
                     }
                 }
-            }
+            //}
+            //catch (Exception err) {
+                // do nothing for right now. most errors will be related to wrong files dropped.
+            //}
         }
 
         private void poolsettings_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelPool_Paint(object sender, PaintEventArgs e)
         {
 
         }
