f52f9ac6d945bee14675bae5fccce4b2da8a7df6
Philip Bawn <pabawn@gmail.com>
2015-01-09 23:08:47 -0800

    added form for authoring question pool questions which will be exported tab delimited

---
+++ GC_BB_Exam_Tool/question-authoring.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace GC_BB_Exam_Tool
+{
+    partial class question_authoring
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.pnlOptions = new System.Windows.Forms.Panel();
+            this.btnSetCorrectAnswer = new System.Windows.Forms.Button();
+            this.btn_Make_MC = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pnlOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(712, 233);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseUp);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(25, 390);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(712, 161);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Question Type";
+            this.columnHeader1.Width = 101;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Question Text";
+            this.columnHeader2.Width = 160;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Paste or create questions below:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Maroon;
+            this.lblStatus.Location = new System.Drawing.Point(25, 352);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(712, 35);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Select new question to create above.";
+            // 
+            // pnlOptions
+            // 
+            this.pnlOptions.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pnlOptions.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlOptions.Controls.Add(this.btnDone);
+            this.pnlOptions.Controls.Add(this.btnSetCorrectAnswer);
+            this.pnlOptions.Controls.Add(this.btn_Make_MC);
+            this.pnlOptions.Controls.Add(this.btnCancel);
+            this.pnlOptions.Location = new System.Drawing.Point(25, 266);
+            this.pnlOptions.Name = "pnlOptions";
+            this.pnlOptions.Size = new System.Drawing.Size(711, 83);
+            this.pnlOptions.TabIndex = 6;
+            // 
+            // btnSetCorrectAnswer
+            // 
+            this.btnSetCorrectAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSetCorrectAnswer.Location = new System.Drawing.Point(171, 3);
+            this.btnSetCorrectAnswer.Name = "btnSetCorrectAnswer";
+            this.btnSetCorrectAnswer.Size = new System.Drawing.Size(162, 38);
+            this.btnSetCorrectAnswer.TabIndex = 6;
+            this.btnSetCorrectAnswer.Text = "Set Correct Answer";
+            this.btnSetCorrectAnswer.UseVisualStyleBackColor = false;
+            this.btnSetCorrectAnswer.Visible = false;
+            this.btnSetCorrectAnswer.Click += new System.EventHandler(this.btnSetCorrectAnswer_Click);
+            // 
+            // btn_Make_MC
+            // 
+            this.btn_Make_MC.BackColor = System.Drawing.Color.Silver;
+            this.btn_Make_MC.Location = new System.Drawing.Point(3, 3);
+            this.btn_Make_MC.Name = "btn_Make_MC";
+            this.btn_Make_MC.Size = new System.Drawing.Size(162, 38);
+            this.btn_Make_MC.TabIndex = 5;
+            this.btn_Make_MC.Text = "New Multiple Choice";
+            this.btn_Make_MC.UseVisualStyleBackColor = false;
+            this.btn_Make_MC.Click += new System.EventHandler(this.btn_Make_MC_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancel.Location = new System.Drawing.Point(339, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(162, 38);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel Question";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.BackColor = System.Drawing.Color.Silver;
+            this.btnDone.Location = new System.Drawing.Point(544, 40);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(162, 38);
+            this.btnDone.TabIndex = 7;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = false;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Correct Answer";
+            this.columnHeader3.Width = 104;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Possible Answers";
+            this.columnHeader4.Width = 246;
+            // 
+            // question_authoring
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 558);
+            this.Controls.Add(this.pnlOptions);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "question_authoring";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Question Authoring";
+            this.Load += new System.EventHandler(this.question_authoring_Load);
+            this.pnlOptions.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Panel pnlOptions;
+        private System.Windows.Forms.Button btnSetCorrectAnswer;
+        private System.Windows.Forms.Button btn_Make_MC;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
---
+++ GC_BB_Exam_Tool/question-authoring.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GC_BB_Exam_Tool
+{
+
+    public partial class question_authoring : Form
+    {
+        public string qAuthorState;
+        public string mc_Question;
+        public Boolean choosing_correctanswer;
+        static List<string> mc_Answers = new List<string>();
+        public int mc_correctAnswer;
+
+        public question_authoring()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void question_authoring_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Make_MC_Click(object sender, EventArgs e)
+        {
+            if (qAuthorState == "answers_multichoice") {
+                btnDone_Click(sender, e);
+            }
+            btnCancel.Visible = true;
+            btnSetCorrectAnswer.Visible = true;
+            qAuthorState = "start_multichoice";
+            lblStatus.Text = "Select new multiple choice question text above.";
+        }
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (richTextBox1.SelectedText != "")
+            {
+                switch (qAuthorState)
+                {
+                    case "start_multichoice":
+                        // start authoring a multiple choice question by selecting a question
+                        mc_Question = richTextBox1.SelectedText;
+                        lblStatus.Text = "Select first multiple choice answer using left mouse button. click set correct answer to set the correct answer for the question.";
+                        qAuthorState = "answers_multichoice";
+                        richTextBox1.DeselectAll();
+                        break;
+                    case "answers_multichoice":
+                        mc_Answers.Add(richTextBox1.SelectedText.Trim());
+                        lblStatus.Text = "Select another multiple choice answer using left mouse button. (" + mc_Answers.Count() + " answers so far.)";
+                        qAuthorState = "answers_multichoice";
+                        richTextBox1.DeselectAll();
+                        if (choosing_correctanswer == true)
+                        {
+                            choosing_correctanswer = false;
+                            mc_correctAnswer = mc_Answers.Count();
+                            MessageBox.Show("Correct answer: #" + mc_correctAnswer + " is " + mc_Answers[mc_correctAnswer-1]);
+                        }
+                        break;
+                        // qAuthorState variable is set to a string we are unprepared for
+                    default:
+                        break;
+
+                }
+            }
+        }
+
+        private void btnSetCorrectAnswer_Click(object sender, EventArgs e)
+        {
+            if (qAuthorState == "answers_multichoice")
+            {
+                choosing_correctanswer = true;
+                lblStatus.Text = "The next answer you select will be marked as correct";
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            mc_Answers.Clear();
+            qAuthorState = "";
+            choosing_correctanswer = false;
+            btnCancel.Visible = false;
+            btnSetCorrectAnswer.Visible = false;
+            lblStatus.Text = "Cancelled. Select new question to create above.";
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            if (qAuthorState == "answers_multichoice")
+            {
+                string allanswers = "";
+                // Currently authoring a multiple choice question, save it.
+
+                ListViewItem addq = new ListViewItem("Multiple Choice");
+                addq.SubItems.Add(mc_Question);
+                addq.SubItems.Add(mc_Answers[mc_correctAnswer - 1]);
+                // add all possible answers to listview
+                foreach (string mcq in mc_Answers)
+                {
+                    allanswers = allanswers + mcq + ",";
+                }
+                addq.SubItems.Add(allanswers);
+                listView1.Items.Add(addq);
+
+                //
+                mc_correctAnswer = -1;
+                mc_Answers.Clear();
+                btnCancel.Visible = false;
+                btnSetCorrectAnswer.Visible = false;
+                lblStatus.Text = "";
+                qAuthorState = "";
+            }
+        }
+
+    }
+}
